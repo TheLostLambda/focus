4f09800d88cb07b42e8ad60dd722d5698a2bd05c
yash <yashghelani08@gmail.com>
2018-10-27 16:19:20 +0100

    left

--- main.lua
+++ main.lua
@@ -10,8 +10,8 @@ end
 
 function love.keypressed(key)
    if player.y == groundLevel and key == "space" then
-      player.vy = player.vy + 200
-   elseif player y == groundLevel and key == "" then
+		player.vy = player.vy + 200
+   end
 end
 
 function love.update(dt)
@@ -23,17 +23,11 @@ function love.update(dt)
    else
      player.vy = player.vy - gravity * dt
    end
-end
-
-function love.update(dt)
-   player.x = player.x + 100 * dt
-   player.y = player.y + player.vy * dt
-   if player.y < groundLevel then
-      player.y = groundLevel
-      player.vy = 0
-   else
-     player.vy = player.vy - gravity * dt
-   end
+    if love.keyboard.isDown("left") then
+		player.x = player.x - 100 * dt
+	elseif love.keyboard.isDown("right") then
+		player.x = player.x + 100 * dt
+	end
 end
    
 function love.draw()
